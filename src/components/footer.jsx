b0e132598c82cd5f2af83175380bd83cc77a162b
lzb446703678 <446703678@qq.com>
2024-03-09 23:23:04 +0800

    1

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
           &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
         </p>
         <p>
-          Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
+          Copyright&nbsp;&copy;&nbsp;2024&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
           <CustomLink to="https://101jc.com/" text="旅者Bin" />
           {siteIcp ? (
